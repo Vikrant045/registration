2a39a7b8b77c9ed1b2e848e006489ca570782a8b
Vikrant045 <vikrant602019@gmail.com>
2024-11-13 17:31:12 +0530

    "Added axios and js-cookie dependencies, updated package-lock.json and package.json files"

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placeimg.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com", // Allowing Imgur
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-export default nextConfig;
+export default nextConfig; // Use export default instead of module.exports
